95415b0b8a89d5ffeea86f4eae09e5078e7b4278
Alpharushin <lionayaz@gmail.com>
2018-08-14 18:58:53 +0300

    Login page works!

--- js/main.js
+++ js/main.js
@@ -61,6 +61,100 @@ $(document).ready(function(){
 		
 	}());
 
+	//Валидация формы логина
+	
+	(function(){
+		var formValidLogin = {
+
+			isValid: true,
+
+			init: function(){
+				this._setUpListeners();
+			},
+
+			_setUpListeners: function(){
+				$('#sendingLoginParams').on('click', formValidLogin._formLoginCheckMethod );
+
+				var formInputs = $("#entryFormInputs").find('input');
+				$.each(formInputs, function(index, val){
+					var formInput = $(val);
+					// Спрятать ошибки
+					
+					input.on('keydown', function(){
+
+						$(".show-error").fadeOut(2000).removeClass('show-error');
+
+					});
+
+					input.on('change', function(){
+
+						$(".show-error").fadeOut(2000).removeClass('show-error');
+
+					});	
+
+				});
+
+				
+
+			},
+
+			_formLoginCheckMethod: function(e){
+	    		e.preventDefault();
+					console.log('Погнали!');
+					//Объявление переменных
+					var entryPageForm = $("#entryFormInputs"),
+							emailVal = $("#emailToEntered").val().trim(),
+							passwordVal = $("#passwordToEntered").val().trim(),
+							adminEmail = "mail@mail.com",
+							adminPassword = "123",
+							mailChecker = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
+							valid = true;
+
+				//Проверка email
+				if ( emailVal.length === 0 ) {
+
+					$("#notEnteredEmail").fadeIn(2000).addClass('show-error');
+					
+					valid = false;
+
+				} else if ( !mailChecker.test(emailVal) ) {
+					
+					$("#incorrectEmail").fadeIn(2000).addClass('show-error');
+					
+					valid = false;
+				
+				} else {
+					
+					$(".show-error").fadeOut(2000).removeClass('show-error');
+
+					//Проверка пароля
+					if ( passwordVal.length === 0 ) {
+
+						$("#notEnteredPassword").fadeIn(2000).addClass('show-error');
+
+						valid = false;
+
+					} else if ( emailVal !== adminEmail || passwordVal !== adminPassword ) {  
+
+						$(".adminAccessDenied").fadeIn(2000).addClass('show-error');
+
+						valid = false;
+					}
+		} 
+					if ( valid === true ) {
+					console.log('Ура!');
+
+					entryPageForm.submit();
+					}
+			}
+		};
+
+		formValidLogin.init();
+
+	}());
+
+
+
 });
 
 
